bc5c3f559c5d080c9bce8c4994a02de6c3cb0a59
yangzhiwei <yangzhiwei@jd.com>
2020-08-15 16:33:57 +0800

    init

---
+++ src/main/java/org/cf/forgot/HelloWorld.java
@@ -0,0 +1,11 @@
+package org.cf.forgot;
+
+/**
+ * @author cf
+ * @date 2020/8/15
+ */
+public class HelloWorld {
+    public static void main(String[] args) {
+        System.out.println("hello world.");
+    }
+}
